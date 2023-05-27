95948c0a1b276fdb9e6175756ba7c4cb84331250
inxbxkumxri <125301881+hxrxsu@users.noreply.github.com>
2023-05-27 15:40:13 +0300

    Add files via upload

---
+++ RSC/RSC/Form7.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RSC
+{
+    public partial class Form7 : Form
+    {
+        public Form7()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form1 fr = new Form1();
+            this.Hide();
+            fr.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+            {
+                listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (listBox2.SelectedIndex != -1)
+            {
+                listBox1.Items.Add("Тренировка в " + dateTimePicker1.Value.ToString() + " " + listBox2.SelectedItem.ToString());
+            }
+        }
+    }
+}
